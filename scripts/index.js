54a5c21d9599d43426f6fc44d3acb7b398d3d2ed
MasterfulPleb <justinmunger027@yahoo.com>
2021-03-01 21:04:30 -0500

    results

--- scripts/index.js
+++ scripts/index.js
@@ -19,25 +19,50 @@ function addFlavor() {
     newFlavor.id = "calcNewFlavor" + calcFlavorCount
     var newInput = document.createElement("input")
     newInput.className = "calcNewFlavorName"
-    newInput.id = "f" + calcFlavorCount
+    newInput.id = "calcFlavor" + calcFlavorCount
     newInput.type = "text"
     newInput.placeholder = "Flavor " + calcFlavorCount
     var newFlavorPercentInput = document.createElement("input")
     newFlavorPercentInput.className = "calcNewFlavorP"
-    newFlavorPercentInput.id = "fp" + calcFlavorCount
+    newFlavorPercentInput.id = "calcFlavorPercent" + calcFlavorCount
     newFlavorPercentInput.type = "text"
     var newP = document.createElement("p")
     newP.className = "calcNewFlavorSign"
-    newP.id = "calcNewFlavorPP" + calcFlavorCount
     newP.innerText = "%"
     newFlavor.appendChild(newInput)
     newFlavor.appendChild(newFlavorPercentInput)
     newFlavor.appendChild(newP)
     document.getElementById("calcFlavors").appendChild(newFlavor)
+    var resultsNewFlavor = document.createElement("div")
+    resultsNewFlavor.className = "calcResultRow"
+    resultsNewFlavor.id = "calcResultFlavor" + calcFlavorCount
+    var resultsNewP1 = document.createElement("p")
+    resultsNewP1.className = "calcResultFlavor"
+    resultsNewP1.id = "calcResultFlavorName" + calcFlavorCount
+    resultsNewP1.innerText = "Flavor " + calcFlavorCount
+    var resultsNewP2 = document.createElement("p")
+    resultsNewP2.className = "calcResultVolume"
+    resultsNewP2.id = "calcResultFlavorVolume" + calcFlavorCount
+    resultsNewP2.innerText = 0
+    var resultsNewP3 = document.createElement("p")
+    resultsNewP3.className = "calcResultMass"
+    resultsNewP3.id = "calcResultFlavorMass" + calcFlavorCount
+    resultsNewP3.innerText = 0
+    var resultsNewP4 = document.createElement("p")
+    resultsNewP4.className = "calcResultPercent"
+    resultsNewP4.id = "calcResultFlavorPercent" + calcFlavorCount
+    resultsNewP4.innerText = 0
+    resultsNewFlavor.appendChild(resultsNewP1)
+    resultsNewFlavor.appendChild(resultsNewP2)
+    resultsNewFlavor.appendChild(resultsNewP3)
+    resultsNewFlavor.appendChild(resultsNewP4)
+    document.getElementById("calcResult").appendChild(resultsNewFlavor)
 }
 function removeFlavor () {
     var removeFlavor = document.getElementById("calcNewFlavor" + calcFlavorCount)
     removeFlavor.parentNode.removeChild(removeFlavor)
+    var resultsRemoveFlavor = document.getElementById("calcResultFlavor" + calcFlavorCount)
+    resultsRemoveFlavor.parentNode.removeChild(resultsRemoveFlavor)
     calcFlavorCount--
 }
 addFlavor()
