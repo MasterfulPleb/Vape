ba67b45fbf7c16021ab444bf301f6389f132444a
MasterfulPleb <justinmunger027@yahoo.com>
2021-03-05 22:11:56 -0500

    cleanup

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
-    height: 900,
+    height: 1100,
     webPreferences: {
       nodeIntegration: true
     },
