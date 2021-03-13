d8f01b4714aa102f7c669d2b85b55961fa427fb5
MasterfulPleb <justinmunger027@yahoo.com>
2021-03-12 20:40:08 -0500

    It Works!

--- scripts/index.js
+++ scripts/index.js
@@ -36,7 +36,7 @@ function addFlavor() {
     newPercentInput.max = "100"
     let newP = newFlavor.appendChild(document.createElement("p"))
     newP.className = "calcNewFlavorSign"
-    newP.innerText = "%"
+    newP.innerHTML = "%"
     
     //  add result flavor
     let resultsNewFlavor = document.getElementById("calcResult").appendChild(document.createElement("div"))
@@ -45,33 +45,26 @@ function addFlavor() {
     let resultsFlavorName = resultsNewFlavor.appendChild(document.createElement("p"))
     resultsFlavorName.id = "calcResultFlavorName" + calcFlavorCount
     resultsFlavorName.className = "calcResultFlavor"
-    resultsFlavorName.innerText = "Flavor " + calcFlavorCount
+    resultsFlavorName.innerHTML = "Flavor " + calcFlavorCount
     let resultsVolume = resultsNewFlavor.appendChild(document.createElement("p"))
     resultsVolume.id = "calcResultFlavorVolume" + calcFlavorCount
     resultsVolume.className = "calcResultVolume"
-    resultsVolume.innerText = 0
+    resultsVolume.innerHTML = "0"
     let resultsMass = resultsNewFlavor.appendChild(document.createElement("p"))
     resultsMass.id = "calcResultFlavorMass" + calcFlavorCount
     resultsMass.className = "calcResultMass"
-    resultsMass.innerText = 0
+    resultsMass.innerHTML = "0"
     let resultsPercent = resultsNewFlavor.appendChild(document.createElement("p"))
     resultsPercent.id = "calcResultFlavorPercent" + calcFlavorCount
     resultsPercent.className = "calcResultPercent"
-    resultsPercent.innerText = 0
+    resultsPercent.innerHTML = "0"
 
-    //  add event listener for flavor name/%
+    //  add event listener for flavor name
     newInput.onchange = () => {
         if (newInput.value == "") {
             resultsFlavorName.innerHTML = "Flavor " + i
         } else {
             resultsFlavorName.innerHTML = newInput.value
-        }
-    }
-    newPercentInput.onchange = () => {
-        if (newPercentInput.value == "") {
-            resultsPercent.innerHTML = "0"
-        } else {
-            resultsPercent.innerHTML = newPercentInput.value
         }
     }
 
@@ -94,6 +87,9 @@ function removeFlavor() {
     //  remove flavor from array
     calcFlavorList.length = calcFlavorCount
     calcResultFlavorList.length = calcFlavorCount
+
+    //  update calculator
+    updateCalc()
 }
 addFlavor()
 
@@ -104,6 +100,7 @@ const calcVG = document.getElementById("calcVG")
 const calcBaseStrength = document.getElementById("calcBaseStrength")
 const calcBasePG = document.getElementById("calcBasePG")
 const calcBaseVG = document.getElementById("calcBaseVG")
+const calcBase = document.getElementById("")
 
 calcPG.onchange = () => calcVG.value = "100" - calcPG.value
 calcVG.onchange = () => calcPG.value = "100" - calcVG.value
@@ -113,5 +110,49 @@ document.getElementById("calcFlavors").onchange = updateCalc
 document.getElementById("calcTopLeft").onchange = updateCalc
 
 function updateCalc() {
-    document.getElementById("calcResultBaseVolume").innerText = "7"
+    let totalFlavorPercent = 0
+    calcFlavorList.forEach(flavor => {
+        if (flavor.getElementsByClassName("calcNewFlavorP")[0].value == "") {
+            totalFlavorPercent += 0
+        } else {
+            totalFlavorPercent += parseFloat(flavor.getElementsByClassName("calcNewFlavorP")[0].value)
+        }
+    })
+    let basePGVGDensity = calcBasePG.value / 100 * 1.036 + calcBaseVG.value / 100 * 1.261
+    let baseDensity = calcBaseStrength.value / 1000 * 1.01 + (1 - calcBaseStrength.value / 1000) * basePGVGDensity
+    let BV = calcAmmount.value * calcStrength.value / calcBaseStrength.value
+    let BM = BV * baseDensity
+    let BP = 100 * BV / calcAmmount.value
+    let PGV = ((calcPG.value - totalFlavorPercent) * calcAmmount.value - calcBasePG.value * BV) / 100
+    let PGM = PGV * 1.036
+    let PGP = 100 * PGV / calcAmmount.value
+    let VGV = (calcVG.value * calcAmmount.value - calcBaseVG.value * BV) / 100
+    let VGM = VGV * 1.261
+    let VGP = 100 * VGV / calcAmmount.value
+    document.getElementById("calcResultBaseVolume").innerHTML = Math.round(BV * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultBaseMass").innerHTML = Math.round(BM * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultBasePercent").innerHTML = Math.round(BP * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultPGVolume").innerHTML = Math.round(PGV * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultPGMass").innerHTML = Math.round(PGM * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultPGPercent").innerHTML = Math.round(PGP * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultVGVolume").innerHTML = Math.round(VGV * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultVGMass").innerHTML = Math.round(VGM * Math.pow(10,2)) / Math.pow(10,2)
+    document.getElementById("calcResultVGPercent").innerHTML = Math.round(VGP * Math.pow(10,2)) / Math.pow(10,2)
+    for (i = 0;  i < calcFlavorCount; i++) {
+        let flavorP = calcFlavorList[i].getElementsByClassName("calcNewFlavorP")[0].value
+        if (flavorP == "") {
+            calcResultFlavorList[i].getElementsByClassName("calcResultPercent")[0].innerHTML = "0"
+            flavorP = parseFloat(flavorP)
+            calcResultFlavorList[i].getElementsByClassName("calcResultVolume")[0].innerHTML = "0"
+            calcResultFlavorList[i].getElementsByClassName("calcResultMass")[0].innerHTML = "0"
+        } else {
+            calcResultFlavorList[i].getElementsByClassName("calcResultPercent")[0].innerHTML = flavorP
+            flavorP = parseFloat(flavorP)
+            let flavorVolume = flavorP * calcAmmount.value / 100
+            let flavorMass = flavorVolume * 1.036
+            calcResultFlavorList[i].getElementsByClassName("calcResultVolume")[0].innerHTML = Math.round(flavorVolume * Math.pow(10,2)) / Math.pow(10,2)
+            //  this is where individual flavor density will need to tie into the calculator
+            calcResultFlavorList[i].getElementsByClassName("calcResultMass")[0].innerHTML = Math.round(flavorMass * Math.pow(10,2)) / Math.pow(10,2)
+        }
+    }
 }
