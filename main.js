4de4e4cdbc7f483f5ba64110d0b7ded82e079527
MasterfulPleb <79616684+MasterfulPleb@users.noreply.github.com>
2021-03-14 14:18:29 -0400

    Revert "Calculator (#11)"
    This reverts commit ffc8a4a2ae2442cd0a38baee8853b917b46a043f.

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 1100,
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true
     },
